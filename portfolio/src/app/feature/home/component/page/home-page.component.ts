ddadaf7c1891846d71d09985a55d8dc9a04d341b
Sandra Herbst <sandraherbst2000@gmail.com>
2024-01-15 21:42:48 +0100

    [PS-3] Removed home page from base project

--- portfolio/src/app/feature/home/component/page/home-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "pw-home-page",
-  templateUrl: "./home-page.component.html",
-  styleUrls: ["./home-page.component.css"],
-})
-export class HomePage {}
