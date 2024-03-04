6c05803611ab1277ee3b27880dde07a682acb3ef
Sandra Herbst <sandraherbst2000@gmail.com>
2024-03-04 21:51:44 +0100

    [PS-4] Added project-details component. Added navigation icons. Implemented tailwind animation keyframes

--- portfolio/src/app/core/interface/side-navigation-component.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface SideNavItemInterface {
-  setInactive(): void;
-}
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
   theme: {
     // add or override certain properties of the default theme with extend keyword
     extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-out-up': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)',
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.25s ease-out',
+        'fade-out-up': 'fade-out-up 0.25s ease-out'
+      },
       screens: {
         'tablet': '640px',
         'laptop': '1024px',
