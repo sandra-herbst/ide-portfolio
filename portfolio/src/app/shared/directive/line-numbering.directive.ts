79a7a9638ba3129aadb3b45359cec63a7abadbb8
Sandra Herbst <sandraherbst2000@gmail.com>
2024-03-18 17:50:26 +0100

    [PS-9] fixed scroll animation, line numbering and scrolling on desktop.

--- portfolio/src/app/shared/directive/line-numbering.directive.ts
+++ portfolio/src/app/shared/directive/line-numbering.directive.ts
@@ -1,64 +1,16 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
-import { LogService } from "../../core/service/log.service";
-import { NavigationService } from "../../core/service/navigation.service";
+import { Directive, ElementRef, OnInit } from "@angular/core";
 
 @Directive({
   selector: "[pwLineNumberingDirective]",
   standalone: true,
 })
 export class LineNumberingDirective implements OnInit {
-  private currentHeight: number | undefined;
-  private lineHeight: number = 21;
-  private offset: number = 92;
+  private MAX_LIMIT: number = 150;
 
-  constructor(
-    private elRef: ElementRef,
-    private renderer: Renderer2,
-    private navService: NavigationService,
-    private logger: LogService
-  ) {}
+  constructor(private elRef: ElementRef) {}
 
   ngOnInit(): void {
-    this.navService.currentRoute$.subscribe(() => {
-      const height = this.elRef.nativeElement.offsetHeight - this.offset;
-      this.onRouteChange(height);
-      this.currentHeight = height;
-    });
-  }
-
-  onRouteChange(height: number): void {
-    this.logger.log("onHeightChange:", height.toString());
-    const newLineCount: number = Math.floor(height / this.lineHeight);
-    if (this.currentHeight == null) {
-      this.increaseNumbering(1, newLineCount);
-      this.currentHeight = height;
-      return;
-    }
-
-    console.log("LineNumberingDirective Height changed:", height);
-    const oldLineCount: number = Math.floor(this.currentHeight / this.lineHeight);
-    const lineDiff: number = Math.abs(oldLineCount - newLineCount);
-    if (this.currentHeight > height) {
-      this.reduceNumbering(lineDiff);
-    } else if (this.currentHeight < height) {
-      const startingIndex = oldLineCount + 1;
-      this.increaseNumbering(startingIndex, lineDiff);
-    }
-    this.currentHeight = height;
-  }
-
-  increaseNumbering(startingIndex: number, lineCountDiff: number): void {
-    console.log("increaseNumbering:", lineCountDiff);
-    const lineNumbers: number[] = this.generateNumberArray(startingIndex, lineCountDiff);
-
-    lineNumbers.forEach(lineNumber => {
-      const tr: HTMLTableRowElement = document.createElement("tr");
-      const td: HTMLTableCellElement = document.createElement("td");
-      td.textContent = lineNumber.toString();
-      td.classList.add("text-gray-100");
-      tr.appendChild(td);
-      this.elRef.nativeElement.insertAdjacentElement("beforeend", tr);
-    });
+    this.increaseNumbering(1, this.MAX_LIMIT);
   }
 
   private generateNumberArray(start: number, increase: number): number[] {
@@ -70,13 +22,16 @@ export class LineNumberingDirective implements OnInit {
     return result;
   }
 
-  reduceNumbering(lineCountDiff: number): void {
-    console.log("reduceNumbering:", lineCountDiff);
-    const container = this.elRef.nativeElement;
-    const children = container.children;
+  increaseNumbering(startingIndex: number, lineCountDiff: number): void {
+    const lineNumbers: number[] = this.generateNumberArray(startingIndex, lineCountDiff);
 
-    for (let i = children.length - 1; i >= children.length - lineCountDiff; i--) {
-      this.renderer.removeChild(container, children[i]);
-    }
+    lineNumbers.forEach(lineNumber => {
+      const tr: HTMLTableRowElement = document.createElement("tr");
+      const td: HTMLTableCellElement = document.createElement("td");
+      td.textContent = lineNumber.toString();
+      td.classList.add("text-gray-100");
+      tr.appendChild(td);
+      this.elRef.nativeElement.insertAdjacentElement("beforeend", tr);
+    });
   }
 }
